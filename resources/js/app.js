094607ddf58d464a74dbb5af349e9ddd59ca8387
hayknazaryann <nazaryanhayk4@gmail.com>
2023-12-19 01:11:23 +0400

    laravel vue vuetify sample app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+
+import { createApp } from 'vue'
+import App from './views/layouts/App.vue'
+
+// Router
+import router  from './router'
+
+// Vuetify
+import vuetify  from './plugins/vuetify.js'
+
+createApp(App)
+    .use(vuetify)
+    .use(router)
+    .mount("#app")
