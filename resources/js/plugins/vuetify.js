6aa1480e0dec3a5158222c0b3ad74d16264a31ab
hayknazaryann <nazaryanhayk4@gmail.com>
2023-12-20 02:06:53 +0400

    auth forms and theme switcher

--- resources/js/plugins/vuetify.js
+++ resources/js/plugins/vuetify.js
@@ -5,12 +5,31 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+const customDarkTheme = {
+    dark: true,
+    colors: {
+
+    },
+};
+
+const customLightTheme = {
+    dark: false,
+    colors: {
+
+    },
+};
+
 const vuetify = createVuetify({
     components,
     directives,
     theme: {
-        defaultTheme: 'dark'
-    }
+        defaultTheme: 'light',
+        themes: {
+            light: customLightTheme,
+            dark: customDarkTheme,
+        },
+    },
+
 })
 
 export default vuetify
